68e36af9071325ab6e2e78b33b0dab582bf5e00a
Eljah Capers <elicasper2000@yahoo.com>
2024-05-29 00:20:32 -0400

    complete

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 main.remove();
 const newHeader = document.createElement("h1");
 newHeader.setAttribute("id", "victory");
-newHeader.textContent = "Elijah is the Champion";
+newHeader.textContent = "Elijah is the champion";
